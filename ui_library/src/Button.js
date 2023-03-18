95887e0d5c18b8d0e66e9012323c9623eb67ec62
ayocodess <ayo.daniel.adesanya@gmail.com>
2023-03-18 22:38:09 +0100

    test

---
+++ ui_library/src/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native-dom";
+
+const Button = () => (
+  <View style={{ backgroundColor: "blue", padding: 10 }}>
+    <Text style={{ color: "white", fontSize: 18 }}>hey</Text>
+  </View>
+);
+
+export default Button;
